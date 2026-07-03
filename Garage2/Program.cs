87780244427a7d69b83ba4a3cf717cb7fa237593
Zabi Sideqi <zabi.sideqi@gmail.com>
2026-07-03 14:35:47 +0200

    Write a commnt to create my branch

--- Garage2/Program.cs
+++ Garage2/Program.cs
@@ -29,7 +29,7 @@ using (var scope = app.Services.CreateScope())
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -45,5 +45,5 @@ app.MapControllerRoute(
     pattern: "{controller=ParkedVehicles}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
+//Komment 
 app.Run();
